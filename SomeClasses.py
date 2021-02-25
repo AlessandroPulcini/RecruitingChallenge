49ab816818ebdcbeb4fe20b48a34543e92cf3498
AlessandroPulcini <79671028+AlessandroPulcini@users.noreply.github.com>
2021-02-26 00:45:57 +0100

    Initial commit

---
+++ SomeClasses.py
@@ -0,0 +1,44 @@
+# Task 8
+class Rectangle: #TODO: fix the class
+    pass
+
+
+class Circle: #TODO: fix the class
+    pass
+
+
+# Task 9
+class Product: #TODO: fix the class
+    pass
+
+
+# Task 10
+class PhoneBook: #TODO: fix the class
+    def add_number(self, name, number):
+        """
+       Add a contact or throw InvalidOperationException if it is already present
+        :param number: an integer number
+        :param name: a string
+       """
+        pass
+
+    def del_number(self, name):
+        """
+        Delete a contact or throw InvalidOperationException if it is not present
+        :param name: a string
+        """
+        pass
+
+    def size(self):
+        """
+        :return: current size of PhoneBook
+        """
+        return None
+
+    def get_all(self):
+        """
+        return: a string representation of all entries in the format 'name: number',
+        merged and separated by ';' and a space. The entries should be sorted alphabetically
+        """
+
+        return ""
