3c0708f4fb3e2e905847806858bcf29bc216db21
AlessandroPulcini <79671028+AlessandroPulcini@users.noreply.github.com>
2021-02-26 16:10:32 +0100

    Update SomeClasses.py

--- SomeClasses.py
+++ SomeClasses.py
@@ -1,44 +1,76 @@
 # Task 8
-class Rectangle: #TODO: fix the class
-    pass
+class Rectangle:
+    def __init__(self, initb, inith):
+        self.b = initb
+        self.h = inith
+    def area(self):
+        return self.b * self.h
 
-
-class Circle: #TODO: fix the class
-    pass
+from math import pi
+class Circle:
+    def __init__(self, initr):
+        self.r = initr
+    def area(self):
+        return self.r * self.r * pi
 
 
 # Task 9
-class Product: #TODO: fix the class
-    pass
-
+class Product:
+    def __init__(self, initn, initp):
+        self.name = initn
+        self.price = initp
+    def __str__(self):
+        return self.name + " costs " + str(self.price) + " euro(s)"
 
 # Task 10
-class PhoneBook: #TODO: fix the class
+class PhoneBook:
+    def __init__(self):
+        self.names = []
+        self.numbers = []
+
     def add_number(self, name, number):
         """
        Add a contact or throw InvalidOperationException if it is already present
         :param number: an integer number
         :param name: a string
        """
-        pass
+        if name not in self.names:
+            pos = next((i for i, record in enumerate(self.names) if name < record ), -1)
+            if pos == -1:
+                self.names.append(name)
+                self.numbers.append(number)
+            else:
+                self.names.insert(pos, name)
+                self.numbers.insert(pos, number)
+        else:
+            pass
 
     def del_number(self, name):
         """
         Delete a contact or throw InvalidOperationException if it is not present
         :param name: a string
         """
-        pass
+        if name in self.names:
+            pos = self.names.index(name)
+            self.names.pop(pos)
+            self.numbers.pop(pos)
+        else:
+            pass
 
     def size(self):
         """
         :return: current size of PhoneBook
         """
-        return None
+        return len(self.names)
 
     def get_all(self):
         """
         return: a string representation of all entries in the format 'name: number',
         merged and separated by ';' and a space. The entries should be sorted alphabetically
         """
-
-        return ""
+        output = ""
+        for i in range(len(self.names)):
+            output += self.names[i] + ": " + str(self.numbers[i])
+            if i != len(self.names) - 1:
+                output += "; "
+        return output
